bd70a76b5ef7d7e748af7924c99875868553d705
Louis <louis.kirkpatrick@landregistry.gov.uk>
2022-12-01 10:31:03 +0000

    updated the creating of the json

--- create_output_file.py
+++ create_output_file.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 
 base_url = 'this_is_a_test'
 
@@ -8,6 +8,7 @@ for file_object in os.scandir('releases'):
   filename = file_object.name
   if filename.endswith('.pdf'):
     link = f'{base_url}{filename}'.replace(' ', '%20')
-    output.append({'name': filename.replace('.pdf', ''), 'link': link})
+    output.append({"name": filename.replace('.pdf', ''), "link": link})
 
-print(output)
+with open('index.json', 'w') as outputfile:
+  json.dumps(output, outputfile)
